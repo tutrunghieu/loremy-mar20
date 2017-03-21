05a080d713f3a12c1feca3fda61b200008e8db83
henrytu <tutrunghieu@gmail.com>
2017-03-21 10:13:51 +0700

    Adding physical

---
+++ app/src/main/java/com/silkroadpacific/loremy_mar20/physical/Tab_Order.java
@@ -0,0 +1,31 @@
+package com.silkroadpacific.loremy_mar20.physical;
+
+/**
+ * Created by Administrator on 3/20/2017.
+ */
+public class Tab_Order extends Tab_Abstract {
+
+    public String orderId ;
+
+    public String couponCode;
+
+    public String itemPhone;
+
+    public String itemMail;
+
+    public String totalAmount;
+
+    public String totalSaleAmount;
+
+    public String token;
+
+    public String itemActionUrl;
+
+    public String orderTotalPrice;
+
+    public Object items;
+
+    public String couponPercent;
+
+}
+
--- app/src/main/java/com/silkroadpacific/loremy_mar20/physical/Tab_Post.java
+++ app/src/main/java/com/silkroadpacific/loremy_mar20/physical/Tab_Post.java
@@ -3,15 +3,19 @@ package com.silkroadpacific.loremy_mar20.physical;
 /**
  * Created by Administrator on 3/20/2017.
  */
-public class Tab_Post {
+public class Tab_Post extends Tab_Abstract {
 
     public String itemId;
 
     public String itemTitle;
 
-    public String itemImageUrl;
+    public Object itemImageUrl;
 
     public String itemRichText;
+
+    public String itemRichTextShort;
+
+    public String itemDetailUrl;
 
 }
 
