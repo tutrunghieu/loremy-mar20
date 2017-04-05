c8d9d55989123010d396a0b98eb2276cdd44bc6d
henrytu <tutrunghieu@gmail.com>
2017-04-05 16:59:07 +0700

    Adding images dynamically

--- loremy/app/src/main/java/com/silkroadpacific/loremy/gui/user/UserFragment.java
+++ loremy/app/src/main/java/com/silkroadpacific/loremy/gui/user/UserFragment.java
@@ -1,15 +1,27 @@
 package com.silkroadpacific.loremy.gui.user;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.silkroadpacific.loremy.R;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 
 public class UserFragment extends Fragment implements View.OnClickListener {
@@ -46,15 +58,64 @@ public class UserFragment extends Fragment implements View.OnClickListener {
 
         for(int k=0; k<100; k++)
         {
-            Button btn = new Button(getActivity());
-            btn.setText("Button " + k);
-            btn.setLayoutParams(new ViewGroup.LayoutParams(
+//            Button btn = new Button(getActivity());
+//            btn.setText("Button " + k);
+//            btn.setLayoutParams(new ViewGroup.LayoutParams(
+//                    ViewGroup.LayoutParams.MATCH_PARENT,
+//                    ViewGroup.LayoutParams.WRAP_CONTENT));
+//            ll.addView(btn);
+
+            TextView tk = new TextView(getActivity());
+
+            tk.setText("Ngoc trinh " + k);
+
+            tk.setLayoutParams(new ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT));
-            ll.addView(btn);
+
+            ll.addView(tk);
+
+            ImageView vk = new ImageView(getActivity());
+
+            Glide.with(getActivity())
+                    .load("http://sohanews.sohacdn.com/k:2016/18-1461210229609/mat-qua-xinh-nhung-ngoc-trinh-se-xau-ho-khi-nhin-lai-hinh-anh-nay.jpg")
+                    .into(vk);
+
+            vk.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            ll.addView(vk);
         }
 
         return v;
+    }
+
+    private Bitmap getBitmap()
+    {
+        String url = "http://sohanews.sohacdn.com/k:2016/18-1461210229609/mat-qua-xinh-nhung-ngoc-trinh-se-xau-ho-khi-nhin-lai-hinh-anh-nay.jpg";
+
+
+        try {
+        URL newurl = new URL(url);
+        return BitmapFactory.decodeStream(newurl.openConnection() .getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Uri getImage()
+    {
+        String url = "http://sohanews.sohacdn.com/k:2016/18-1461210229609/mat-qua-xinh-nhung-ngoc-trinh-se-xau-ho-khi-nhin-lai-hinh-anh-nay.jpg";
+        try {
+            //InputStream is = (InputStream) new URL(url).getContent();
+            return Uri.parse(url);
+            //return Drawable.createFromStream(is, "src name");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
